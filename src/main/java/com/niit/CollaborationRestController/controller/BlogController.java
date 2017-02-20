58d1242669bcab6d5dd11c1755dac7fb29caf886
santoshenoy <santoshenoy@gmail.com>
2017-02-21 00:16:48 +0530

    21st Feb

---
+++ src/main/java/com/niit/CollaborationRestController/controller/BlogController.java
@@ -0,0 +1,60 @@
+package com.niit.CollaborationRestController.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.CollaborationBackEnd.dao.BlogDAO;
+import com.niit.CollaborationBackEnd.model.Blog;
+import com.niit.CollaborationBackEnd.model.User;
+
+@RestController
+public class BlogController {
+
+	@Autowired
+	Blog blog;
+
+	@Autowired
+	BlogDAO blogDAO;
+
+	@PostMapping("/addBlog/")
+	public ResponseEntity<Blog> addBlog(@RequestBody Blog blog) {
+		blogDAO.save(blog);
+		blog.setErrorCode("200");
+		blog.setErrorMsg("SUCCESS");
+		return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+	}
+
+	@GetMapping("/listBlog")
+	public ResponseEntity<List<Blog>> getList() {
+		List<Blog> list = blogDAO.list();
+		blog.setErrorCode("200");
+		blog.setErrorMsg("Success.....");
+		return new ResponseEntity<List<Blog>>(list, HttpStatus.OK);
+	}
+
+	@GetMapping("/delete_blog-{id}")
+	public ResponseEntity<Blog> deleteBlog(@PathVariable("id") String id) {
+		blogDAO.delete(id);
+		blog.setErrorCode("200");
+		blog.setErrorMsg("Deleted Successfully........");
+		return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+	}
+
+	@PutMapping("/updateBlog")
+	public ResponseEntity<Blog> editBlog(@RequestBody Blog blog) {
+		blogDAO.update(blog);
+		blog.setErrorCode("200");
+		blog.setErrorMsg("Edited Successfully.....");
+		return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+	}
+
+}
--- src/main/java/com/niit/CollaborationRestController/controller/UserController.java
+++ src/main/java/com/niit/CollaborationRestController/controller/UserController.java
@@ -1,38 +1,59 @@
 package com.niit.CollaborationRestController.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.niit.CollaborationBackEnd.dao.Test_UserDAO;
 import com.niit.CollaborationBackEnd.dao.UserDAO;
-import com.niit.CollaborationBackEnd.model.Test_User;
 import com.niit.CollaborationBackEnd.model.User;
 
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	User user;
-	
+
 	@Autowired
 	UserDAO userDAO;
-	
-	
+
 	@PostMapping("/addUser/")
 	public ResponseEntity<User> addUser(@RequestBody User user) {
-		
 		userDAO.save(user);
 		user.setErrorCode("200");
 		user.setErrorMsg("SUCCESS");
-		
-		return new ResponseEntity<User>(user,HttpStatus.OK);
-		
-		
+		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
-	
+
+	@GetMapping("/listUser")
+	public ResponseEntity<List<User>> getList() {
+		List<User> list = userDAO.list();
+		user.setErrorCode("200");
+		user.setErrorMsg("Success.....");
+		return new ResponseEntity<List<User>>(list, HttpStatus.OK);
+	}
+
+	@GetMapping("/delete_user-{id}")
+	public ResponseEntity<User> deleteUser(@PathVariable("id") String id) {
+		userDAO.delete(id);
+		user.setErrorCode("200");
+		user.setErrorMsg("Deleted Successfully........");
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+
+	@PutMapping("/updateUser")
+	public ResponseEntity<User> editUser(@RequestBody User user) {
+		userDAO.update(user);
+		user.setErrorCode("200");
+		user.setErrorMsg("Edited Successfully.....");
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
 
 }
